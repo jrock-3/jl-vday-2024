3d008c9b371f52a3a69778832c3aad849ca4d295
Justin Liao <justin.liao3@gmail.com>
2024-03-07 21:28:08 -0800

    Change vite base url to be consistent with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jl-jl-2024/',
+  base: '/jlxjl/',
   plugins: [react()],
   build: {
     target: 'es2022',
